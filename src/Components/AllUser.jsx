50f24f1cfd9bb8208251109f4b3326a057cff6a3
Md. Abdur Rahman <46405817+Riad2019@users.noreply.github.com>
2021-10-19 01:15:51 +0600

    done

--- src/Components/AllUser.jsx
+++ src/Components/AllUser.jsx
@@ -3,8 +3,44 @@ import { useState, useEffect } from 'react';
 import { Table, TableHead, TableCell, TableRow, TableBody, Button, makeStyles } from '@material-ui/core'
 import { Link } from 'react-router-dom';
 import { getUsers, deleteUser } from '../service/api';
-import Search from './Search';
 
+import { Box, InputBase } from '@material-ui/core';
+import { Search as SearchIcon } from '@material-ui/icons';  
+
+const useStyles2 = makeStyles(theme => ({
+    component: {
+        background: '#F6F6F6',
+        height: 43,
+        display: 'flex',
+        alignItems: 'center',
+        transition: 'box-shadow .18s ease-out,background-color .25s ease-out'
+    },
+    search: {
+        position: 'relative',
+        borderRadius: 18,
+        backgroundColor: '#FFFFFF',
+        margin: '0 13px' ,
+        width: '100%'
+      },
+      searchIcon: {
+        color: '#919191',
+        padding: theme.spacing(0, 2),
+        height: '100%',
+        position: 'absolute',
+        display: 'flex',
+        alignItems: 'center'
+      },
+      inputRoot: {
+        width: '100%'
+      },
+      inputInput: {
+        padding: theme.spacing(1, 1, 1, 0),
+        paddingLeft: 65,
+        fontSize: 14,
+        height: 15,
+        width: '100%'
+    }
+}));
 
 const useStyles = makeStyles({
     table: {
@@ -29,29 +65,66 @@ const useStyles = makeStyles({
 
 const AllUser=()=>{
 
-
+    const classes1 = useStyles2();
     const classes = useStyles();
     const [users,setUsers] = useState([]);
+    const [filtered ,setFilterd] = useState([]);
+    const [result , setResult] = useState("");
     
     useEffect(()=>{
+        
         getAllUsers();
     },[])
 
+
+    useEffect(()=> {
+        const results = filtered.filter(res=> res.name.toLowerCase().includes(result)
+
+        ); 
+        setUsers(results)
+    } ,[result])
+
+
+
     const deleteUserData =async(id)=>{
         await deleteUser(id);
+       
         getAllUsers();
 
     }
 
-const getAllUsers = async()=>{
-    let response = await getUsers()
-    setUsers(response.data);
-}
+    const getAllUsers = async()=>{
+
+        let response = await getUsers()
+        setUsers(response.data);
+        setFilterd(response.data);
+    }
+    
+    const onChange =(e)=> {
+        setResult(e.target.value);
+    }
 
 
     return(
         <>
-        <Search/>
+          <Box className={classes1.component}>
+        <Box className={classes1.search}>
+            <Box className={classes1.searchIcon}>
+              <SearchIcon fontSize="small"/>
+            </Box>
+            <InputBase
+              placeholder="Search by name."
+              classes={{
+                root: classes1.inputRoot,
+                input: classes1.inputInput,
+              }}
+              inputProps={{ 'aria-label': 'search' }}
+              onChange={onChange}
+            />
+          </Box>
+        </Box>
+
+        
         <Table className={classes.table}>
         <TableHead>
             <TableRow className={classes.thead}>
